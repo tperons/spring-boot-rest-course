778ca2a8b2f0c2fc76be9d9d2afc472f7db1d746
tperons <tperons@gmail.com>
2026-02-25 17:21:15 -0300

    feat(project): initialize Spring Boot application

---
+++ src/main/java/com/tperons/SpringBootRestCourseApplication.java
@@ -0,0 +1,13 @@
+package com.tperons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tperons/SpringBootRestCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tperons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
